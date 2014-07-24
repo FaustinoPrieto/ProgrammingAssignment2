0c4f546bb09697c702359f66a4f80e60bd485f6d
FaustinoPrieto <faustino.prieto@unican.es>
2014-07-24 12:44:42 +0200

    Revert "R Code"
    This reverts commit 02a6a129a314fec2487ad15322455998a6fe6e51.

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,15 @@
-## Course: R Programming. Programming Assignment 2. 24/07/2014
-## A function able to cache Matrix inversion time-consuming computations.
+## Put comments here that give an overall description of what your
+## functions do
 
-## This function creates a special "matrix" object that can cache its inverse.
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setsolve <- function(solve) 
-                m <<- solve
-        getsolve <- function() m
-        list(set = set, get = get,
-             setsolve = setsolve,
-             getsolve = getsolve)
+
 }
 
 
-## This function computes the inverse of the special "matrix".
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getsolve()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setsolve(m)
-        m
 }
